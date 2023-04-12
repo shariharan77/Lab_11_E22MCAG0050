027cbca2c92954641774126ede7c498b2f60fbd8
shariharan77 <123491199+shariharan77@users.noreply.github.com>
2023-04-12 18:50:42 +0530

    Add files via upload

---
+++ dough.java
@@ -0,0 +1,34 @@
+//Hariharan S
+//E22MCAG0050
+import java.util.Scanner;
+
+class Crust {
+    private String name;
+    private double cost;
+    
+    public Crust(String name, double cost) {
+        this.name = name;
+        this.cost = cost;
+    }
+    
+    public String getName() {
+    return name;
+}
+
+public double getCost() {
+    return cost;
+}
+
+public static Crust selectCrust(Scanner input, String name) {
+    System.out.print("What type of crust would you like, " + name + "? (thin, thick, or stuffed) ");
+    String crust_name = input.nextLine();
+    switch (crust_name.toLowerCase()) {
+        case "thick":
+            return new Crust("Thick", 1.99);
+        case "stuffed":
+            return new Crust("Stuffed", 2.99);
+        default:
+            return new Crust("Thin", 0.00);
+    }
+}
+}
---
+++ pizzaclass.java
@@ -0,0 +1,31 @@
+//Hariharan S
+//E22MCAG0050
+import java.util.Scanner;
+
+class Pizza {
+private String style;
+private Cheese cheese;
+private Sauce sauce;
+private Crust crust;
+private int num_toppings;
+public Pizza(String style, Cheese cheese, Sauce sauce, Crust crust, int num_toppings) {
+    this.style = style;
+    this.cheese = cheese;
+    this.sauce = sauce;
+    this.crust = crust;
+    this.num_toppings = num_toppings;
+}
+
+public double getCost() {
+    double cost = 5.00; // base cost of pizza
+    
+    if (style.equalsIgnoreCase("gourmet")) {
+        cost += 2.00; // additional cost for gourmet style
+    }
+    
+    cost += cheese.getCost() + sauce.getCost() + crust.getCost();
+    cost += num_toppings * 0.50; // additional cost per topping
+    
+    return cost;
+}
+}
